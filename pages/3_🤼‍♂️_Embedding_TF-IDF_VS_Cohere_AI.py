924c2cb8bc3e2b9e79dcac01af50dce134a4d847
simon.ouyang <souyang6608@gmail.com>
2023-04-18 11:51:43 -0700

    add persist option

--- pages/3_🤼‍♂️_Embedding_TF-IDF_VS_Cohere_AI.py
+++ pages/3_🤼‍♂️_Embedding_TF-IDF_VS_Cohere_AI.py
@@ -61,7 +61,7 @@ def load_data_with_nltk_preprocessing(rowNumber=constants.MAX_ROWS):
         data = data
     return data
 
-# @st.cache_data(show_spinner=True)
+@st.cache_data(persist="disk", show_spinner=True)
 def setEmbeddedClassificationTFIDF():
     data = load_data_with_nltk_preprocessing()
     corpus = []
@@ -85,7 +85,7 @@ def setEmbeddedClassificationTFIDF():
         tfidf_accuracy[utils.print_estimator_name(classifier)] = score
     return tfidf_accuracy
 
-# @st.cache_data(show_spinner=True)
+@st.cache_data(persist="disk", show_spinner=True)
 def setEmbeddedClassificationCohere():
     # Splitting the testing and training sets
     # Build a pipeline of model for four different classifiers.
